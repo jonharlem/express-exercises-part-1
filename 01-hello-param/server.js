b5fa2b336e7dab676d61470db9536ac013043ef9
Jon Harlem <jtharlem@gmail.com>
2015-12-08 16:18:38 -0800

    completed hello-param

--- 01-hello-param/server.js
+++ 01-hello-param/server.js
@@ -3,9 +3,22 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    // write your response here.
+	res.send("Hello, Express!")
 });
 
+// app.get('/harlem' function(req, res) {
+// 	res.send("Hello, Harlem!")
+// })
+
+app.get('/:name', function(req, res) {
+	var words = req.params.name.split("-");
+	var capWords = [];
+	for (var i = 0; i < words.length; i++) {
+		capWords.push(words[i][0].toUpperCase() + words[i].slice(1))
+	};
+	res.send("Hello, " + capWords.join(" ") + "!")
+})
+
 app.listen(8000, function() {
-    console.log("Listening on port 8000...");
+	console.log("Listening on port 8000...");
 });
